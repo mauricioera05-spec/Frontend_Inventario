40f98940a3da169f93ff31177eab43585eadfe31
MauricioERA <144066836+MauricioERA@users.noreply.github.com>
2026-09-18 17:46:45 -0600

    Configurar despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mi-primer-proyecto', //Nombre del repositorio
+  base: '/mi-primer-proyecto/', //Nombre del repositorio
 })
